ec6688cd598b6eb79eb2920d91d5e5a254673782
matias1364 <matiasleoncontreras@gmail.com>
2023-10-24 20:45:09 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  useEffect(async()=>{
+
+    const url = 'https://tennisapi1.p.rapidapi.com/api/tennis/events/live';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'b4444ff329mshc073181333238cep115507jsn36b44ba70186',
+    'X-RapidAPI-Host': 'tennisapi1.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+  },[])
+
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
