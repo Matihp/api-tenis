894d367af296096304f88328975c061e8fa19c2e
matias1364 <matiasleoncontreras@gmail.com>
2023-10-26 20:55:57 -0300

    add card

---
+++ src/components/cardPlayer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CardPlayer = ({name1,name2}) => {
+
+
+
+    return ( 
+        <div>
+            <p>{name1},{name2}</p>
+        </div>
+     );
+}
+ 
+export default CardPlayer;
